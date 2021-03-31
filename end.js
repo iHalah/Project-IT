eb0863e9059de82a73d6db8f9d712b8ffbb7c7d4
Halah <iHalah5@gmail.com>
2021-03-31 16:27:31 +0300

    LastPart| udemy

--- end.js
+++ end.js
@@ -2,9 +2,11 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById("finalScore");
+const MAX_HIGH_SCORES = 5;
+
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
+//console.log(highScores);
 
 finalScore.innerText = mostRecentScore;
 
@@ -21,5 +23,14 @@ saveHighScore = e => {
         score: mostRecentScore,
         name: username.value
     };
-    console.log(score);
+
+    highScores.push(score);
+    highScores.sort( (a, b) => b.score - a.score);
+
+    highScores.splice(MAX_HIGH_SCORES);
+    //Rearrange
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
+
+    //console.log(highScores);
 };
--- game.js
+++ game.js
@@ -12,32 +12,54 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href= 'xxx.js'>",
-        choice2: "<script name= 'xxx.js'>",
-        choice3: "<script src= 'xxx.js'>",
-        choice4: "<script file= 'xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+
+//JSON file                 //Respone from json file
+
+fetch('questions.json')
+    .then(res => {
+        return res.json();
+    }).then(loadedQuestions => {
+        console.log(loadedQuestions);
+        questions = loadedQuestions;
+        startGame();
+    }) .catch (err => {console.error(err);})
+
+//Question from API
+/*
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+*/
+
 
 
 const CORRECT_BONUS = 10;
@@ -48,7 +70,7 @@ startGame = function () {
     questionCounter = 0;
     score = 0;
     availableQuestions = [ ...questions];
-    console.log(availableQuestions);
+    //console.log(availableQuestions);
     getNewQuestion();
 };
 
@@ -109,5 +131,5 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
+
 
